6141da1a60f70fc80bd8ee23c119ea1b002f4ef0
Anil Rautela <arautela@xavient.com>
2017-03-01 16:23:07 +0530

    Dashboard Modified

--- Controller/DashboardController.js
+++ Controller/DashboardController.js
@@ -4,4 +4,16 @@ scotchApp.controller('index', function($scope) {
 
 scotchApp.controller('home', function($scope) {
     
+    $scope.url = "#/home";
+});
+
+scotchApp.controller('calender', function($scope) {
+    
+    $scope.url = "#/calender";
+});
+
+
+scotchApp.controller('profile', function($scope) {
+   
+    $scope.url = "#/profile";
 });
--- Controller/Router.js
+++ Controller/Router.js
@@ -4,9 +4,21 @@ scotchApp.config(function($routeProvider) {
     $routeProvider
 
     // route for the home page
-        .when('/', {
+        .when('/home', {
         templateUrl: '/../pages/home.html',
         controller: 'home'
     })
+    
+     // route for the home page
+        .when('/profile', {
+        templateUrl: '/../pages/profile.html',
+        controller: 'profile'
+    })
+    
+    // route for Calender page
+        .when('/calender', {
+        templateUrl: '/../pages/CalendarRoute.html',
+        controller: 'calender'
+    });
 
 });
